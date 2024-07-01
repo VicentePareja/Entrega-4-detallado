688beba0f63aeec55604859ebd1db69c67ae1a8d
Vicente Pareja <vicente.pareja@uc.cl>
2024-06-30 20:02:11 -0400

    skills cleaned

--- Fire-Emblem/Fire-Emblem/Controller/Skills/DamageAlterations/AbsoluteDamageReduction/AxeGuard.cs
+++ Fire-Emblem/Fire-Emblem/Controller/Skills/DamageAlterations/AbsoluteDamageReduction/AxeGuard.cs
@@ -13,10 +13,9 @@ public class AxeGuard : DamageAlterationSkill
 
     public override void ApplyEffect(Battle battle, Character owner)
     {
-        _counterTimes++;
+        SetAttributes(battle, owner);
         if (_counterTimes % 2 == 0)
         {
-            SetAttributes(battle, owner);
             ApplyDamageEffect();
         }
     }
@@ -26,6 +25,7 @@ public class AxeGuard : DamageAlterationSkill
         _owner = owner;
         _opponent = (_combat._attacker == owner) ? _combat._defender : _combat._attacker;
         _isOpponentAxe = _opponent.Weapon == "Axe";
+        _counterTimes++;
     }
     
     private void ApplyDamageEffect()
--- Fire-Emblem/Fire-Emblem/obj/Debug/net8.0/Fire-Emblem.AssemblyInfo.cs
+++ Fire-Emblem/Fire-Emblem/obj/Debug/net8.0/Fire-Emblem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fire-Emblem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da17b2cf394e474de849416d04bb43d42fe92930")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63d81b8e58b32381982c3138ba45eb8d06975c4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fire-Emblem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fire-Emblem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
